19e79e653443a1fb9c9aa36689213b278778657c
ipz231pmo <ipz231_pmo@student.ztu.edu.ua>
2023-11-06 16:34:49 +0200

    Task 2

---
+++ LinearExpressions2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter n: ");
+double n = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter m: ");
+double m = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter x: ");
+double x = double.Parse(Console.ReadLine());
+
+double
+    z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(m * m * m * n) + m * n + m * m - m),
+    z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m,
+    y = 2.4 * Math.Abs((x * x + b) / a) + (a-b)*Math.Pow(Math.Sin(a-b), 2) + Math.Pow(10, -2)*(x-b);
+Console.WriteLine("z1 is " + z1.ToString());
+Console.WriteLine("z1 is " + z2.ToString());
+Console.WriteLine("z1 is " + y.ToString());
